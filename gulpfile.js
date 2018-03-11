3796076101e0caafbaaaa3d46f36d14f23fcd157
Marvin Lange <MLA@timetoact.de>
2018-03-11 17:44:50 +0100

    simple heroku express run

--- gulpfile.js
+++ gulpfile.js
@@ -187,11 +187,3 @@ gulp.task('watch', function () {
 
   gulp.watch(['build/**'], browserSync.reload);
 });
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: ["build"],
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
-});
