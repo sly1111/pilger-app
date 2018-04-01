a238ca2e7bf61727891a402b3a160c650efeac07
Marvin Lange <MLA@timetoact.de>
2018-04-01 14:49:38 +0200

    add remove option

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'build'));
 app.set('view engine', 'pug');
 app.use(express.static('build'));
 app.get('/', (req, res) => render.getFileNames(res));
-app.get('/removeImages', (req, res) => google.removeImages());
+app.get('/s4wFG0bQmRvQREvb1PUW', (req, res) => google.removeImages());
 app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), () =>
 console.log(`Listening on ${app.get('port')}`)
